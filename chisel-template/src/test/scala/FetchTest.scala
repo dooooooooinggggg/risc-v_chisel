c756a0d90be7dfae2228901cc4b6f8fd2a0337dc
Tatsunori Ishikawa <dooooooooinggggg@gmail.com>
2021-12-02 17:37:52 +0900

    cp from book

--- chisel-template/src/test/scala/FetchTest.scala
+++ chisel-template/src/test/scala/FetchTest.scala
@@ -1,18 +1,15 @@
 package fetch
 
 import chisel3._
-
-// ChiselTestを利用するために必要なpackage
 import org.scalatest._
 import chiseltest._
 
 class HexTest extends FlatSpec with ChiselScalatestTester {
-    "mycpu" should "work through hex" in {
-        test(new Top) { c =>
-            // このブロックでテストを記述(変数cはTopクラスのインスタンス)
-            while (!c.io.exit.peek().litToBoolean) {
-                c.clock.step(1)
-            }
-        }
+  "mycpu" should "work through hex" in {
+    test(new Top) { c =>
+      while (!c.io.exit.peek().litToBoolean){
+        c.clock.step(1)
+      }
     }
+  }
 }
